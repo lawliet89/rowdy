c318ff196795d77c7ca31c0ada9c28e02491cdc8
Yong Wen Chua <me@yongwen.xyz>
2017-03-08 17:00:08 +0800

    Implement all the CORS request headers

--- src/cors.rs
+++ src/cors.rs
@@ -11,6 +11,7 @@ use std::str::FromStr;
 
 use hyper::Url;
 use hyper::error::ParseError;
+use rocket;
 use rocket::request::{self, Request, FromRequest};
 use rocket::response::{self, Response, Responder};
 use rocket::http::{Method, Status};
@@ -21,6 +22,9 @@ use rocket::Outcome;
 pub enum Error {
     MissingOrigin,
     BadOrigin(ParseError),
+    MissingRequestMethod,
+    BadRequestMethod(rocket::Error),
+    MissingRequestHeaders,
 }
 
 impl error::Error for Error {
@@ -28,6 +32,15 @@ impl error::Error for Error {
         match *self {
             Error::MissingOrigin => "The request header `Origin` is required but is missing",
             Error::BadOrigin(_) => "The request header `Origin` contains an invalid URL",
+            Error::MissingRequestMethod => {
+                "The request header `Access-Control-Request-Method` \
+                 is required but is missing"
+            }
+            Error::BadRequestMethod(_) => "The request header `Access-Control-Request-Method` has an invalid value",
+            Error::MissingRequestHeaders => {
+                "The request header `Access-Control-Request-Headers` \
+                is required but is missing"
+            }
         }
     }
 
@@ -43,12 +56,14 @@ impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             Error::BadOrigin(ref e) => fmt::Display::fmt(e, f),
+            Error::BadRequestMethod(ref e) => fmt::Debug::fmt(e, f),
             _ => write!(f, "{}", error::Error::description(self)),
         }
     }
 }
 
-/// The `Origin` header used in CORS
+/// The `Origin` request header used in CORS
+#[derive(Debug)]
 pub struct Origin(Url);
 
 impl FromStr for Origin {
@@ -63,10 +78,10 @@ impl FromStr for Origin {
 impl<'a, 'r> FromRequest<'a, 'r> for Origin {
     type Error = Error;
 
-    fn from_request(request: &'a Request<'r>) -> request::Outcome<Origin, Error> {
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Error> {
         match request.headers().get_one("Origin") {
             Some(origin) => {
-                match Origin::from_str(origin) {
+                match Self::from_str(origin) {
                     Ok(origin) => Outcome::Success(origin),
                     Err(e) => Outcome::Failure((Status::BadRequest, Error::BadOrigin(e))),
                 }
@@ -76,11 +91,63 @@ impl<'a, 'r> FromRequest<'a, 'r> for Origin {
     }
 }
 
-/// The `Access-Control-Request-Method` header
+/// The `Access-Control-Request-Method` request header
+#[derive(Debug)]
 pub struct AccessControlRequestMethod(Method);
 
-/// The `Access-Control-Request-Headers`
+impl FromStr for AccessControlRequestMethod {
+    type Err = rocket::Error;
+
+    fn from_str(method: &str) -> Result<Self, Self::Err> {
+        Ok(AccessControlRequestMethod(Method::from_str(method)?))
+    }
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for AccessControlRequestMethod {
+    type Error = Error;
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Error> {
+        match request.headers().get_one("Access-Control-Request-Method") {
+            Some(request_method) => {
+                match Self::from_str(request_method) {
+                    Ok(request_method) => Outcome::Success(request_method),
+                    Err(e) => Outcome::Failure((Status::BadRequest, Error::BadRequestMethod(e))),
+                }
+            }
+            None => Outcome::Failure((Status::BadRequest, Error::MissingRequestMethod)),
+        }
+    }
+}
+
+/// The `Access-Control-Request-Headers` request header
+#[derive(Debug)]
 pub struct AccessControlRequestHeaders(HashSet<String>);
+
+/// Will never fail
+impl FromStr for AccessControlRequestHeaders {
+    type Err = ();
+
+    fn from_str(headers: &str) -> Result<Self, Self::Err> {
+        let set: HashSet<String> = headers.split(',').map(|header| header.trim().to_string()).collect();
+        Ok(AccessControlRequestHeaders(set))
+    }
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for AccessControlRequestHeaders {
+    type Error = Error;
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Error> {
+        match request.headers().get_one("Access-Control-Request-Headers") {
+            Some(request_headers) => {
+                match Self::from_str(request_headers) {
+                    Ok(request_headers) => Outcome::Success(request_headers),
+                    Err(()) => unreachable!("`AccessControlRequestHeaders::from_str` should never fail"),
+                }
+            }
+            None => Outcome::Failure((Status::BadRequest, Error::MissingRequestHeaders)),
+        }
+    }
+}
 
 
 /// The CORS type, which implements `Responder`. This type allows
@@ -201,22 +268,66 @@ impl<'r, R: Responder<'r>> Responder<'r> for CORS<R> {
 #[cfg(test)]
 #[allow(unmounted_route)]
 mod tests {
+    use std::collections::HashSet;
+    use std::str::FromStr;
+
+    use hyper;
     use rocket;
     use rocket::testing::MockRequest;
     use rocket::http::Method::*;
-    use rocket::http::Header;
+    use rocket::http::{Header, Status};
 
-    use cors;
+    use cors::*;
 
     #[get("/hello")]
-    fn hello() -> cors::CORS<&'static str> {
-        cors::CORS::any("Hello, world!")
+    fn hello() -> CORS<&'static str> {
+        CORS::any("Hello, world!")
     }
 
     #[get("/request_headers")]
-    fn request_headers(origin: cors::Origin) -> cors::CORS<String> {
-        let cors::Origin(origin) = origin;
-        cors::CORS::any(origin.into_string())
+    fn request_headers(origin: Origin,
+                       method: AccessControlRequestMethod,
+                       headers: AccessControlRequestHeaders)
+                       -> String {
+        let Origin(origin) = origin;
+        let AccessControlRequestMethod(method) = method;
+        let AccessControlRequestHeaders(headers) = headers;
+        format!("{}\n{}\n{}", origin, method, headers.iter().cloned().collect::<Vec<String>>().join(", "))
+    }
+
+    #[test]
+    fn origin_header_parsing() {
+        let url = "https://foo.bar.xyz";
+        not_err!(Origin::from_str(url));
+
+        let url = "https://foo.bar.xyz/path/somewhere"; // this should never really be used
+        not_err!(Origin::from_str(url));
+
+        let url = "invalid_url";
+        is_err!(Origin::from_str(url));
+    }
+
+    #[test]
+    fn request_method_parsing() {
+        let method = "POST";
+        let parsed_method = not_err!(AccessControlRequestMethod::from_str(method));
+        assert_matches!(parsed_method, AccessControlRequestMethod(Method::Post));
+
+        let method = "options";
+        let parsed_method = not_err!(AccessControlRequestMethod::from_str(method));
+        assert_matches!(parsed_method, AccessControlRequestMethod(Method::Options));
+
+        let method = "INVALID";
+        is_err!(AccessControlRequestMethod::from_str(method));
+    }
+
+    #[test]
+    fn request_headers_parsing() {
+        let headers = ["foo", "bar", "baz"];
+        let parsed_headers = not_err!(AccessControlRequestHeaders::from_str(&headers.join(", ")));
+        let expected_headers: HashSet<String> = headers.iter().map(|s| s.to_string()).collect();
+        let AccessControlRequestHeaders(actual_headers) = parsed_headers;
+        assert_eq!(actual_headers, expected_headers);
     }
 
     #[test]
@@ -225,6 +336,7 @@ mod tests {
         let mut req = MockRequest::new(Get, "/hello");
         let mut response = req.dispatch_with(&rocket);
 
+        assert_eq!(Status::Ok, response.status());
         let body_str = response.body().and_then(|body| body.into_string());
         let values: Vec<_> = response.header_values("Access-Control-Allow-Origin").collect();
         assert_eq!(values, vec!["*"]);
@@ -232,13 +344,25 @@ mod tests {
     }
 
     #[test]
-    fn request_headers_smoke_test() {
+    fn request_headers_round_trip_smoke_test() {
         let rocket = rocket::ignite().mount("/", routes![request_headers]);
-        let origin_header = Header::new("Origin", "https://foo.bar.xyz");
-        let mut req = MockRequest::new(Get, "/request_headers").header(origin_header);
+        let origin_header = Header::from(not_err!(hyper::header::Origin::from_str("https://foo.bar.xyz")));
+        let method_header = Header::from(hyper::header::AccessControlRequestMethod(hyper::method::Method::Get));
+        let request_headers = hyper::header::AccessControlRequestHeaders(vec![FromStr::from_str("accept-language")
+                                                                                  .unwrap(),
+                                                                              FromStr::from_str("X-Ping").unwrap()]);
+        let request_headers = Header::from(request_headers);
+        let mut req = MockRequest::new(Get, "/request_headers")
+            .header(origin_header)
+            .header(method_header)
+            .header(request_headers);
         let mut response = req.dispatch_with(&rocket);
 
+        assert_eq!(Status::Ok, response.status());
         let body_str = not_none!(response.body().and_then(|body| body.into_string()));
-        println!("{}", body_str);
+        let expected_body = r#"https://foo.bar.xyz/
+GET
+accept-language, X-Ping"#;
+        assert_eq!(expected_body, body_str);
     }
 }
--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,7 @@
 macro_rules! not_err {
     ($e:expr) => (match $e {
         Ok(e) => e,
-        Err(e) => panic!("{} failed with {}", stringify!($e), e),
+        Err(e) => panic!("{} failed with {:?}", stringify!($e), e),
     })
 }
 
