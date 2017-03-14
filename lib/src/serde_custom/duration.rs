fb171aea4b3af5b2d1a9083e710626244e8ae6b8
Yong Wen Chua <me@yongwen.xyz>
2017-03-14 13:58:10 +0800

    Custom duration serde tests

--- lib/src/serde_custom/duration.rs
+++ lib/src/serde_custom/duration.rs
@@ -1,3 +1,4 @@
+//! Custom serializer and deserializer for `std::time::Duration`. Serializes to seconds, and deserializes from seconds.
 use std::time::Duration;
 use serde::{Deserialize, Serializer, Deserializer};
 
@@ -12,4 +13,31 @@ pub fn deserialize<D>(deserializer: D) -> Result<Duration, D::Error>
 {
     let duration = u64::deserialize(deserializer)?;
     Ok(Duration::from_secs(duration))
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+    use serde_json;
+    use super::*;
+
+    #[derive(Serialize, Deserialize, Eq, PartialEq, Debug)]
+    struct TestStruct {
+        #[serde(with = "super")]
+        duration: Duration
+    }
+
+    #[test]
+    fn serialization_round_trip() {
+        let structure = TestStruct {
+            duration: Duration::from_secs(1234)
+        };
+
+        let expected_json = "{\"duration\":1234}";
+        let actual_json = not_err!(serde_json::to_string(&structure));
+        assert_eq!(expected_json, actual_json);
+
+        let deserialized_struct: TestStruct = not_err!(serde_json::from_str(&actual_json));
+        assert_eq!(structure, deserialized_struct);
+    }
 }
